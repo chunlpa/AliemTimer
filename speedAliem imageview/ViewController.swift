44e86d5623326b91ecfdc46de92ebbeb01926062
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-04-04 15:46:22 +0900

    AlienTimer

--- speedAliem imageview/ViewController.swift
+++ speedAliem imageview/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+var counter = 1
+var myTimer = Timer()
+var check = true
+var checked = true   //타이머 객체 생성
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var index: UILabel!
+    
+  
+  override func viewDidLoad() {
+  
+     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+          @IBAction func play(_ sender: Any) {
+        if checked == true {
+            
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+            checked = false
+            
+        } else if checked == false{
+            myTimer.invalidate()
+            checked = true
+        }
+          }
+   
+    
+    
+    @objc func doAnimation() {
+        if counter == 5 {
+            check = false
+        } else if counter == 1 {
+            check = true
+        }
+        if check == true {
+            counter = counter + 1
+        } else {
+            counter = counter - 1
+        }
+        //타이머에 의해 동적으로 호출되는 함수
+        imageView.image = UIImage(named: "frame\(counter).png")
+        index.text = String(counter)
+}
 
 
 }
-
